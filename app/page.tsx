48c7995c4d70fb23300cab66d0c4111a5fa1aeed
Jacob MacLeod <jmacleod142@gmail.com>
2024-07-10 15:34:11 -0400

    change from Node to Next

---
+++ app/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import Image from "next/image";
+
+export default function Home() {
+
+  function downloadFile() {
+    var filename = window.prompt("Please enter the file name: ", "filename.txt");
+    var content = (document.getElementById("notepad") as HTMLInputElement)?.value;
+    var element = document.createElement('a');
+    var blob = new Blob([content], {type: 'text/plain'});
+    var fileUrl = URL.createObjectURL(blob);
+    
+    if (filename != null) {
+      var isTxt = filename?.endsWith(".txt");
+      if (!isTxt) {
+        filename = filename + ".txt";
+      }
+    
+      element.setAttribute('href', fileUrl);
+      element.setAttribute('download', filename);
+      element.style.display = 'none';
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+    }
+  }
+
+  return (
+    <html>
+      <head>
+      </head>
+      <body>
+        <button id="download" onClick={downloadFile}>Download file</button>
+        <textarea id="notepad" name="notepad"></textarea>
+        <script type="text/javascript">
+        </script>
+      </body>
+    </html>
+  );
+}
